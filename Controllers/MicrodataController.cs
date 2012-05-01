42e470850e0e1cd15d3b3d466692b0414e712059
Nicholas Hillier <nicholas@ghostmonk.com>
2012-04-30 23:12:51 -0400

    Only one page created... pretty much static, but it's a decent start

---
+++ Controllers/MicrodataController.cs
@@ -0,0 +1,37 @@
+﻿using System.Web.Mvc;
+
+namespace MicrodataMaker.Controllers
+{
+    public class MicrodataController : Controller
+    {
+        public ActionResult Person()
+        {
+            return View();
+        }
+
+        public ActionResult Event()
+        {
+            return View();
+        }
+        
+        public ActionResult Organization()
+        {
+            return View();
+        }
+
+        public ActionResult Product()
+        {
+            return View();
+        }
+
+        public ActionResult Review()
+        {
+            return View();
+        }
+
+        public ActionResult Offer()
+        {
+            return View();
+        }
+    }
+}
